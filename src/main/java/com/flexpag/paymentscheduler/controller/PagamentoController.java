0c234ff626a2cacfe3443413ed9cdc3ebea2b18e
Barbara Ayres <ayresbarbara2@gmail.com>
2022-10-13 13:49:00 -0300

    Repository

---
+++ src/main/java/com/flexpag/paymentscheduler/controller/PagamentoController.java
@@ -0,0 +1,14 @@
+package com.flexpag.paymentscheduler.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/pagamento")
+@CrossOrigin ("*")
+public class PagamentoController {
+	
+	
+
+}
